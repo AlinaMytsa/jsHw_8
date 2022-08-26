288b8338c5d614387cbee9773a24e61fe4216d08
AlinaMytsa <act.mitsa@gmail.com>
2022-08-26 20:39:48 +0300

    update

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@
 let num = 1;
 
 //2
-num += num + 12;
+num += 12;
 
 //3
-num -= num - 14;
+num -= 14;
 
 //4
-num *= num * 5;
+num *= 5;
 
 //5
-num /= num / 7;
+num /= 7;
 
 //6
-num = ++num + 1;
+++num;
 
 //7
-num = --num - 1;
+--num;
 
 // result
 alert(num);
